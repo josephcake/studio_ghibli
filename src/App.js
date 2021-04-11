9dbfc8210ef23c13f047b0cc4fe873e35e557e8f
Joseph Gao <joseph8p@gmail.com>
2021-04-11 19:06:29 -0400

    initial fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+
 const App = () => {
+
+  useEffect(() => {
+
+    fetch(`https://ghibliapi.herokuapp.com/films`)
+    .then(resq=>resq.json())
+    .then(resp=>console.log(resp))    
+  }, [])
+
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
+  
 
-  console.log("state:", state);
-  console.log("dispatch:", dispatch);
+  // console.log("state:", state);
+  // console.log("dispatch:", dispatch);
+
   
   return (
       <div className='App'>
