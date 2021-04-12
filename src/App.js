b56ecfe9e867e7b643176137dced47e3c3df1d6f
Joseph Gao <joseph8p@gmail.com>
2021-04-11 22:58:42 -0400

    initiate modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
 import Navigation from './navigation/Navigation'
+import Modal from './modal/Modal'
 
 const App = () => {
-
+  const [displayModal, setDisplayModal] = useState(null) 
   useEffect(() => {
     //endpoings
     //films,people,locations,species,vehicles
-    fetch(`https://ghibliapi.herokuapp.com/films`)
+    fetch(`https://ghibliapi.herokuapp.com/films?limit=50`)
     .then(resq=>resq.json())
     .then(resp=>console.log(resp))    
-  }, [])
 
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
+    //check localStorage
+    if(localStorage.preview){
+      setDisplayModal(1)
+    }else{
+
+    }
+
+  }, [])
+  // const state = useSelector((state) => state);
+  // const dispatch = useDispatch();
   // console.log("state:", state);
   // console.log("dispatch:", dispatch);  
+
+
   return (
       <div className='App'>
         <Navigation/>
+        {displayModal??<Modal/>}
       </div>
   );
 }
---
+++ src/modal/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Overlay } from './Modal.styled'
+
+const Modal = () =>{
+  return(
+    <Overlay>
+    </Overlay>
+  )
+}
+
+export default Modal
