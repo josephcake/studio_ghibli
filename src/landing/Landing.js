39efae63d6094bcc49e798ed2eb6eb9e8443d39c
Joseph Gao <joseph8p@gmail.com>
2021-04-20 21:58:49 -0400

    news a-tag added

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,5 +1,6 @@
 import React, { createRef, useEffect, useState } from "react";
 import styled from "styled-components";
+import {newsInfo} from './constant'
 
 const LandingContainer = styled.div`
   width: 100vw;
@@ -28,8 +29,9 @@ const LandingFooterText = styled.h1`
   margin-left:20px;
 `
 const LandingNewsContainer = styled.div`
-  border: solid lightgray 1px;
-  height:50%;
+  /* border: solid lightgray 1px; */
+  padding:10px;
+  height:40%;
   width:100%;
   display:flex;
   justify-content:space-between;
@@ -37,30 +39,56 @@ const LandingNewsContainer = styled.div`
 `
 
 const NewsContainer = styled.div`
-  width:50%;
-  height:100%;
-`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction:column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 const News = styled.div`
   width: 100%;
+  display:flex;
+  justify-content: space-between;
+  align-items:center;
 `;
-const NewsTitle = styled.h1`
-  font-size:16px;
-`
+const NewsTitle = styled.a`
+  font-weight: 400;
+  font-size: clamp(10px, 14px, 18px);
+  letter-spacing: 1px;
+  color: white;
+  background-color: black;
+  width: 35%;
+  padding: 7px;
+  border-radius: 7px;
+  text-indent: 10px;
+  margin: 0;
+  text-decoration: none;
+  &:visited {
+    color: white;
+  }
+`;
 const NewsSubtitle = styled.h1`
-  font-size: 16px;
+  font-weight: 200;
+  font-size: clamp(10px, 14px, 18px);
+  letter-spacing: 1px;
+  width: 65%;
+  padding: 7px;
+  margin: 0;
 `;
 
 const Landing = () =>{
+  const news = newsInfo.map((n)=>
+    <News key={n.title}>
+      <NewsTitle href={n.href} target={"_blank"}>{n.title}</NewsTitle>
+      <NewsSubtitle>{n.subtitle}</NewsSubtitle>
+    </News>
+  )
   return (
     <LandingContainer>
       <LandingNewsContainer>
         <NewsContainer>
-          <News>
-            <NewsTitle>Movie "Earwig and the Witch"</NewsTitle>
-            <NewsSubtitle>
-              April 29 (Thursday / holiday) National Road Show
-            </NewsSubtitle>
-          </News>
+          {news}
         </NewsContainer>
       </LandingNewsContainer>
       <LandingFooter>
--- src/landing/constant.js
+++ src/landing/constant.js
@@ -199,4 +199,49 @@ const filmInfo = [
   },
 ];
 
-export {filmInfo}
+
+const newsInfo = [
+  {
+    title: "Movie: 'Earwig and the Witch'",
+    subtitle: "April 29 (Thursday / holiday) National Road Show",
+    href: "https://www.aya-and-the-witch.jp/",
+  },
+  {
+    title: "Stage 'Spirited Away'",
+    subtitle: "February, March 2022 to be performed at the Imperial Theater",
+    href: "https://www.tohostage.com/spirited_away/",
+  },
+  {
+    title: "Studio Ghibli works: Still Image",
+    subtitle: "Added 228 scene photos of Studio Ghibli works!",
+    href: "https://www.ghibli.jp/info/013409/",
+  },
+  {
+    title: "Ghibli Museum Mitaka",
+    subtitle: "Official website of Ghibli Museum, Mitaka",
+    href: "https://www.ghibli-museum.jp/",
+  },
+  {
+    title: "Ghibli Exposition",
+    subtitle:
+      "Held at the Aichi Prefectural Museum of Art from Saturday , July 17th !",
+    href: "https://www.ghibli.jp/event/expo/",
+  },
+  {
+    title: "Isao Takahata Exhibition",
+    subtitle: "Held at Fukuoka Art Museum from Thursday, April 29th !",
+    href: "https://www.ghibli.jp/event/takahata/",
+  },
+  {
+    title: "Animage and Ghibli Exhibition",
+    subtitle: "Held at Matsuya Ginza from Thursday , April 15th !",
+    href: "https://www.ghibli.jp/event/animage/",
+  },
+  {
+    title: "Studio Ghibli related links",
+    subtitle: "North America Ghibli goods purchase site added",
+    href: "https://www.ghibli.jp/links/",
+  },
+];
+
+export {filmInfo, newsInfo}
