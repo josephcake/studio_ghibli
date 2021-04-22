ba6fcbde56f5169b1fffa68702ec580ddfead5c4
Joseph Gao <joseph8p@gmail.com>
2021-04-21 21:02:15 -0400

    info constant

---
+++ src/info/constant.js
@@ -0,0 +1,73 @@
+const LatestInfo = [
+  {
+    date: "2021.04.09",
+    title: "'News from Ghibli from Nonaka-kun' April 2021 issue",
+    url: "https://www.ghibli.jp/info/013452/",
+  },
+  {
+    date: "2021.03.10",
+    title: "'News from Ghibli from Nonaka-kun' March 2021 issue",
+    url: "https://www.ghibli.jp/info/013443/",
+  },
+  {
+    date: "2021.03.05",
+    title:
+      "'Howl's Moving Castle' and 'Earthsea' will be broadcast at the 'Friday Road Show' on April 2nd and 9th.",
+    url: "https://www.ghibli.jp/info/013441/",
+  },
+  {
+    date: "2021.02.10",
+    title: "'News from Ghibli from Nonaka-kun' February 2021 issue",
+    url: "https://www.ghibli.jp/info/013431/",
+  },
+  {
+    date: "2021.01.15",
+    title:
+      "From February 20th, 'My Neighbor Totoro' will be screened at the movie town Chofu Cinema Festival.",
+    url: "https://www.ghibli.jp/info/013427/",
+  },
+  {
+    date: "2021.01.15",
+    title:
+      "The new Kabuki 'Nausicaa of the Valley of the Wind' will be released on Blu-ray and DVD on January 20th.",
+    url: "https://www.ghibli.jp/info/013426/",
+  },
+  {
+    date: "2021.01.08",
+    title: "'News from Ghibli from Nonaka-kun' January 2021 issue",
+    url: "https://www.ghibli.jp/info/013425/",
+  },
+];
+
+const EventInfo = [
+  {
+    date: "2021.07.17",
+    title: "Ghibli Exposition (Aichi Prefectural Museum of Art)",
+    url: "https://www.ghibli.jp/event/expo/",
+  },
+
+  {
+    date: "2021.04.29",
+    title: "Isao Takahata Exhibition (Fukuoka Art Museum)",
+    url: "https://www.ghibli.jp/event/takahata/",
+  },
+
+  {
+    date: "2021.04.15",
+    title: "Animage and Ghibli Exhibition (Matsuya Ginza)",
+    url: "https://www.ghibli.jp/event/animage/",
+  },
+
+  {
+    date: "TBD",
+    title: "Ghibli 'Not big' exposition",
+    url: "https://www.ghibli.jp/event/daijanaiexpo/",
+  },
+  {
+    date: "TBD",
+    title: "Toshio Suzuki and Studio Ghibli Exhibition",
+    url: "https://www.ghibli.jp/event/suzuki/",
+  },
+];
+
+export {LatestInfo, EventInfo}
