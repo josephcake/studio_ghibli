3308a59b35e75e3d9e6e87f21f156a9c540f04cd
Joseph Gao <joseph8p@gmail.com>
2021-04-23 23:11:01 -0400

    films posters add

--- src/films/Films.js
+++ src/films/Films.js
@@ -1,19 +1,123 @@
-import React from 'react';
+import React, { createRef, useEffect, useState } from "react";
 import styled from "styled-components";
+import { posters, filmInfo } from "./constant";
+
 import Poster from './Posters'
 
-const FilmsContainer = styled.div`
+
+const MediaContainer = styled.div`  
+`
+const FilmTrailer = styled.div`
   width:100vw;
-  height:100vh;
+  height:calc(100vh - 360px);
+`;
+
+const FilmsContainer = styled.div`
+  height:320px;
+  width:100vw;
+  background-color:black;
+  display:flex;
+  position: relative;
+
+`
+const PosterContainer = styled.div`
+  background-color: black;
+  display: flex;
+  align-items: center;
+  overflow-x: scroll;  
+`;
+
+const LArrowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100%;
+  position: absolute;
+  width:30px;
+  left: 0;
+`;
+const RArrowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100%;
+  position: absolute;
+  width: 30px;
+  right: 0 ;
+`;
+
+const Arrow = styled.h1`
+  margin:0 10px;
+  color:white;
 `
 
 
-const Films = () => {
-  return (
-    <FilmsContainer>
 
-    </FilmsContainer>
-  )
+const YoutubeVid = styled.div`
+  height: calc(100vh - 360px);
+  width: 100vw;
+  display: block;
+`;
+const Overlay = styled.div`
+  position: absolute;
+  /* background-color:rgba(0,0,0,0.3); */
+  top: 80px;
+  height: calc(100vh - 160px);
+  width: 100vw;
+  display: block;
+  z-index: 9999999;
+`;
+
+const Films = () => {
+  const p = posters.map((img)=>(
+    <Poster img={img}/>
+  ))
+  const vidRef = createRef();
+  const iframeRef = createRef();
+  const [vidDimension, setVidDimension] = useState({ h: 0, w: 0 });
+  // const randomlySelectedVid = Math.floor(Math.random() * filmInfo.length - 1);
+
+  useEffect(() => {
+    const h = vidRef.current.offsetHeight.toString();
+    const w = vidRef.current.offsetWidth.toString();
+    setVidDimension((prevState) => ({
+      ...prevState,
+      h,
+      w,
+    }));
+  }, []);
+  return (
+    <MediaContainer>
+      <YoutubeVid ref={vidRef}>
+        {/* <Overlay /> */}
+        <iframe
+          ref={iframeRef}
+          width={vidDimension.w}
+          height={vidDimension.h}
+          // src={`https://www.youtube-nocookie.com/embed/${filmInfo[randomlySelectedVid].url}?autoplay=1&mute=1&showinfo=0&controls=1`}
+          src={`https://www.youtube-nocookie.com/embed/uBnRTMGRMkM?playlist=uBnRTMGRMkM&autoplay=1&mute=1&showinfo=0&controls=1&loop=1`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          allowFullScreen
+        ></iframe>
+      </YoutubeVid>
+
+      <FilmsContainer>
+        <LArrowContainer>
+          <Arrow>{"<"}</Arrow>
+        </LArrowContainer>
+        <RArrowContainer>
+          <Arrow>{">"}</Arrow>
+        </RArrowContainer>
+        <PosterContainer>{p}</PosterContainer>
+      </FilmsContainer>
+    </MediaContainer>
+  );
 }
 
 export default Films
--- src/films/Posters.js
+++ src/films/Posters.js
@@ -6,10 +6,18 @@ const PosterContainer = styled.div`
 
 `
 
-const Poster = () =>{
+const PosterImg = styled.img`
+  height:300px;
+  width:200px;
+  object-fit:cover;
+  object-position:center;
+  margin:0 3px;
+`
+
+const Poster = ({img}) =>{
   return (
   <PosterContainer>
-
+    <PosterImg src={img}/>
   </PosterContainer>
   )
 }
