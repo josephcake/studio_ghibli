a3d032a0b14b8e673a353ba53d9d74493ce32de7
Joseph Gao <joseph8p@gmail.com>
2021-04-25 22:00:53 -0400

    added modal to film, missing close button

--- src/modal/Modal.styled.js
+++ src/modal/Modal.styled.js
@@ -2,12 +2,17 @@ import styled from "styled-components";
 
 const Overlay = styled.div`
   z-index: 999999;
-  position: absolute;
+  position: fixed;
+  /* overflow:hidden; */
+  height:100vh;
+  width:100vw;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: rgba(0, 0, 0, 0.6);
+  /* background-color: rgba(0, 0, 0, 0.6); */
+  background-color: white;
+  /* display:none; */
 `;
 
 export { Overlay };
