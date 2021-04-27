ae73bc267f7ce56c1ff275b2673fb517ecdf48f7
Joseph Gao <joseph8p@gmail.com>
2021-04-26 23:11:26 -0400

    graves of the fireflies and modal faded layover

--- src/films/PosterInfo.js
+++ src/films/PosterInfo.js
@@ -8,7 +8,7 @@ const PosterInfoContainer = styled.div`
   flex-direction:column;
   width: 100%;
   height:100%;
-  overflow-y:auto;
+  overflow-y:hidden;
 `;
 
 const Overview = styled.div`
@@ -17,17 +17,41 @@ const Overview = styled.div`
 `
 const PosterImg = styled.div`
   /* width:30%; */
-  height:100%;
-`
+  height: 100%;
+  border-radius: 20px 0 0 20px;
+  position: relative;
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0) 10%,
+    rgba(0, 0, 0, 0.8) 100%
+  );
+`;
 const Img = styled.img`
-  max-width:680px;
-  max-height:100%;
-`
+  max-width: 45vw;
+  min-width: 45vw;
+  min-height: 80vh;
+  max-height: 80vh;
+  object-position: center;
+  object-fit: cover;
+  border-radius: 20px 0 0 20px;
+`;
+const ImgOverlay = styled.div`
+  position: absolute;
+  top:0;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 0%,    
+    rgba(255, 255, 255, 1) 100%
+  );
+`;
 
 const PosterInfo = ({film}) =>{
-  const id = film.id.toString();
+  const id = film.id
   console.log(allPosters);
-  const posters = allPosters[id-1].default.map((p)=>
+  const gallery = allPosters[id-1].default.map((p)=>
         <PosterImg key={p}>
           <Img src={p}/>
         </PosterImg>
@@ -37,10 +61,11 @@ const PosterInfo = ({film}) =>{
     <PosterInfoContainer>
       <Overview>
         <PosterImg>
-          <Img src={film.poster} />
+          {/* <Img src={film.poster} /> */}
+          <Img src={allPosters[id-1].default[0]} />
+          <ImgOverlay className={"overlay"}/>
         </PosterImg>
       </Overview>
-      {posters}
     </PosterInfoContainer>
   ) : null;
 }
--- src/modal/Modal.styled.js
+++ src/modal/Modal.styled.js
@@ -3,18 +3,25 @@ import styled from "styled-components";
 const Overlay = styled.div`
   z-index: 999999;
   position: fixed;
+  height:100vh;
   width:100vw;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  /* background-color: rgba(130, 130, 130, 1); */
-  background-color: gray;
+  background-color: rgba(0, 0, 0, 1);
+  /* background-color: gray; */
+  display:flex;
+  justify-content:center;
+  align-items:center;
 `;
 
 const Inner = styled.div`
-  height:100%;
-  width:100vw;
+  /* height:100%; */
+  background-color:white;
+  height:80%;
+  width:90%;
+  border-radius:20px;
 `
 const CloseModal = styled.div`
   cursor: pointer;
@@ -24,12 +31,12 @@ const CloseModal = styled.div`
   width: 100px;
   height: 20px;
   background: rgb(0, 0, 0);
-  background: linear-gradient(
+  /* background: linear-gradient(
     90deg,
     rgba(0, 0, 0, 0) 0%,
     rgba(0, 0, 0, 0) 10%,
     rgba(0, 0, 0, 0.8) 100%
-  );
+  ); */
   border-radius: 5px;
   display: flex;
   justify-content: center;
@@ -37,14 +44,14 @@ const CloseModal = styled.div`
   color: white;
   padding: 10px;
   &:hover {
-    background: rgb(50, 50, 50);
-    background: linear-gradient(
+    color:black;
+    background: rgb(220, 220, 220);
+    /* background: linear-gradient(
       90deg,
       rgba(80, 80, 80, 0) 0%,
       rgba(80, 80, 80, 0) 10%,
       rgba(80, 80, 80, 0.8) 100%
-    );
-    color: black;
+    ); */
   }
 `;
 
